add1ed93ff4e133fb7b067f8ed0f5d3ebc51dcae
Davi Silva <davi.silva@usabit.com.br>
2025-06-09 11:28:32 -0300

    fix: uri issue

--- kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/players/BaseAudioPlayer.kt
+++ kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/players/BaseAudioPlayer.kt
@@ -38,7 +38,7 @@ import com.doublesymmetry.kotlinaudio.models.PositionChangedReason
 import com.doublesymmetry.kotlinaudio.notification.NotificationManager
 import com.doublesymmetry.kotlinaudio.players.components.PlayerCache
 import com.doublesymmetry.kotlinaudio.players.components.getAudioItemHolder
-import com.doublesymmetry.kotlinaudio.utils.isUriLocal
+import com.doublesymmetry.kotlinaudio.utils.isUriLocalFile
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.DefaultLoadControl
 import com.google.android.exoplayer2.DefaultLoadControl.Builder
@@ -544,7 +544,7 @@ abstract class BaseAudioPlayer internal constructor(
                 raw.open(DataSpec(uri))
                 DataSource.Factory { raw }
             }
-            isUriLocal(uri) -> {
+            isUriLocalFile(uri) -> {
                 DefaultDataSourceFactory(context, userAgent)
             }
             else -> {
